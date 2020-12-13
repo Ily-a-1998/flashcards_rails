d76445798395a975ef091c1b6a7dd110069924ba
Ily-a-1998 <illlya2018@mail.ru>
2020-12-13 15:58:49 +0600

    rubocop

--- app/controllers/flashcards_controller.rb
+++ app/controllers/flashcards_controller.rb
@@ -1,7 +1,6 @@
 class FlashcardsController < ApplicationController
   def index
-    @card = 
-    if current_user && current_user.current_deck.nil?
+    @card = if current_user && current_user.current_deck.nil?
       Card.dated.sort_random.last
     elsif current_user.decks.find_current_deck.present?
       current_user.current_deck.cards.dated.sort_random.last
