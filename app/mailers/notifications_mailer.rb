ad7a41ad604b8524c5a257cb05290205144f9fd3
Ily-a-1998 <illlya2018@mail.ru>
2021-01-10 12:47:28 +0600

    fix hound

--- app/mailers/notifications_mailer.rb
+++ app/mailers/notifications_mailer.rb
@@ -1,5 +1,4 @@
 class NotificationsMailer < ApplicationMailer
-
   def pending_cards(user)
     @email = user.email
     mail to: @email, subject: 'Карточки, которые ожидают пересмотра'
--- spec/mailers/previews/notifications_mailer_preview.rb
+++ spec/mailers/previews/notifications_mailer_preview.rb
@@ -1,5 +1,4 @@
 class NotificationsMailerPreview < ActionMailer::Preview
-
   def pending_cards
     NotificationsMailer.pending_cards
   end
